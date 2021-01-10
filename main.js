722ebf9b0c2a2f5aec217452972d825f2c866990
Olgasinkevich <sinkevichweb@gmail.com>
2021-01-10 16:01:11 +0300

    add lesson-4 function sum function colorArrayByClick

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ for (let i = 0; i < arr2.length; i++) {
 */
 
 // lesson-3
+/*
 const string = 'word';
 const arr = string.split('');
 const reverse = arr.reverse();
@@ -68,3 +69,110 @@ function max(a, b) {
   return b;
 }
 console.log(max(3, 4));
+*/
+
+// lesson-4
+
+/* function sum (a) {
+    return function (b) {
+    return a+b;
+  }
+} */
+
+// сколько угодно значений (раз) возвращать
+
+function sum(a) {
+  /*  console.log(a);
+    return (b) => sum(a + b); */
+  let result = a;
+  const f = (b) => {
+    result += b;
+    console.log(result);
+    return f;
+  };
+  return f;
+}
+
+sum(5)(2)(3);
+
+// 1 вариант
+/*
+const text1=document.getElementById('text1');
+text1.addEventListener('click',() => {
+  text1.style.color='white';
+  const body=document.getElementsByTagName('body')[0];
+  body.style.backgroundColor = 'grey';
+});
+const text2=document.getElementById('text2');
+text2.addEventListener('click',() => {
+  text2.style.color='red';
+});
+const text3=document.getElementById('text3');
+text3.addEventListener('click',() => {
+  text3.style.color='white';
+});
+*/
+
+// 2 вариант
+/*
+const body=document.getElementsByTagName('body')[0];
+body.style.backgroundColor = 'grey';
+function colorByClick(id, color = 'white') {
+  const element=document.getElementById(id);
+  element.addEventListener('click',() => {
+    element.style.color = color;
+  });
+}
+colorByClick('text1');
+colorByClick('text2', 'red');
+colorByClick('text3');
+*/
+
+// для одного абзаца
+/*
+function colorArrayByClick() {
+  const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+  let counter = undefined;
+  const text1 = document.getElementById('text1');
+  text1.addEventListener('click', ()=>{
+    if (typeof counter === "undefined") {
+      counter = 0;
+    }
+    else {
+      counter++;
+    }
+    text1.style.color = colors[counter];
+    }
+    )
+}
+colorArrayByClick();
+*/
+
+// для любого количества абзацев
+
+function colorArrayByClick(arr) {
+  const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+  arr.forEach((item) => {
+    let counter;
+    const element = document.getElementById(item);
+    element.addEventListener('click', () => {
+      if (counter === undefined || counter >= colors.length) {
+        counter = 0;
+      }
+      /*
+          if (counter === undefined) {
+              counter = 0;
+            } else {
+                      if (counter < colors.length) {
+                counter++;
+            } else {
+              counter=0;
+              }
+            } */
+      element.style.color = colors[counter];
+      counter++;
+    });
+  });
+}
+
+colorArrayByClick(['text1', 'text2', 'text3']);
