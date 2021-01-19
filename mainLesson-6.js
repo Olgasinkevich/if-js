a6facb7a8e173018af13277e4a7f0e478fe81870
Olgasinkevich <sinkevichweb@gmail.com>
2021-01-20 00:36:58 +0300

    add lesson-6 function palindrome function getCalendarMonth

--- mainLesson-6.js
+++ mainLesson-6.js
@@ -1,36 +1,33 @@
 /*
 const word = prompt('Enter a word');
-/*
+
 const reverse =word.split('').reverse().join('');
 alert(`Is this word palindrome? ${word === reverse ? 'Yes' : 'No'}`);
 */
-/*
 const checkPalindrome = (str) => str === str.split('').reverse().join('');
-alert(checkPalindrome(word));
-*/
+alert(checkPalindrome('word'));
 
-/*
-const daysInMonth = 30;
-const daysInWeek = 7;
-const dayOfWeek = 4;//prompt('Enter day of week')
-*/
 const getСalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
-  const daysWithOutTail = daysInMonth + dayOfWeek; // общее количество дней в массиве с учетом только предыдущ месяца
+  const daysWithOutTail = daysInMonth + dayOfWeek;
+  // общее количество дней в массиве с учетом только предыдущ месяца
   const iterations = daysWithOutTail + (daysInWeek - (daysWithOutTail % daysInWeek));
-// общее кол дней в массиве с учетом предыдущ и след месяца
-  let currentDay = daysInMonth - dayOfWeek;
+  // общее кол дней в массиве с учетом предыдущ и след месяца
+  let currentDay = daysInMonth - (dayOfWeek - 1);
+  // т к отсчет элементов в массиве начинается с 0
+  let subArrayIndex = 0;
   const result = [[]];
   for (let i = 1; i <= iterations; i++) {
-    currentDay += 1;
     if (currentDay > daysInMonth) {
-      startDay = 1;
-      return result;
+      currentDay = 1;
     }
     if (result[subArrayIndex].length === daysInWeek) {
-      subArrayIndex += 1;
+      subArrayIndex++;
+      result[subArrayIndex] = [];
     }
     result[subArrayIndex].push(currentDay);
+    currentDay++;
   }
+  return result;
 };
 
 console.log(getСalendarMonth(30, 7, 4));
