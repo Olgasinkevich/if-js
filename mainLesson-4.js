50d367f190c165ca35d56a8748a3cbdb8e9482ef
Olgasinkevich <sinkevichweb@gmail.com>
2021-02-19 23:50:07 +0300

    Lesson-4 add querySelectorAll

--- mainLesson-4.js
+++ mainLesson-4.js
@@ -27,61 +27,8 @@ const callback = (element) => {
 };
 const colorArrayByClick = (arr) => {
   arr.forEach((item) => {
-    const element = document.getElementById(item);
-    element.addEventListener('click', callback(element));
+    item.addEventListener('click', callback(item));
   });
 };
-colorArrayByClick(['text1', 'text2', 'text3']);
-
-// 1 вариант
-/*
-const text1=document.getElementById('text1');
-text1.addEventListener('click',() => {
-  text1.style.color='white';
-  const body=document.getElementsByTagName('body')[0];
-  body.style.backgroundColor = 'grey';
-});
-const text2=document.getElementById('text2');
-text2.addEventListener('click',() => {
-  text2.style.color='red';
-});
-const text3=document.getElementById('text3');
-text3.addEventListener('click',() => {
-  text3.style.color='white';
-});
-*/
-
-// 2 вариант
-/*
-const body=document.getElementsByTagName('body')[0];
-body.style.backgroundColor = 'grey';
-function colorByClick(id, color = 'white') {
-  const element=document.getElementById(id);
-  element.addEventListener('click',() => {
-    element.style.color = color;
-  });
-}
-colorByClick('text1');
-colorByClick('text2', 'red');
-colorByClick('text3');
-*/
-
-// для одного абзаца
-/*
-function colorArrayByClick() {
-  const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-  let counter = undefined;
-  const text1 = document.getElementById('text1');
-  text1.addEventListener('click', ()=>{
-    if (typeof counter === "undefined") {
-      counter = 0;
-    }
-    else {
-      counter++;
-    }
-    text1.style.color = colors[counter];
-    }
-    )
-}
-colorArrayByClick();
-*/
+const querySelectorAll = document.querySelectorAll('.text');
+colorArrayByClick(querySelectorAll);
