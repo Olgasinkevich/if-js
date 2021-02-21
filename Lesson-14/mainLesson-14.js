c37121e3ae05bc29927386bc847c8d70b2ca3fa0
Olgasinkevich <sinkevichweb@gmail.com>
2021-02-21 17:42:25 +0300

    Lesson-14 implement fetch

--- Lesson-14/mainLesson-14.js
+++ Lesson-14/mainLesson-14.js
@@ -1,17 +1,22 @@
+const PATH = 'https://fe-student-api.herokuapp.com/api/file';
 const formEl = document.getElementById('form');
-const inputEl = formEl.querySelectorAll('input');
-const data = {};
 
-formEl.addEventListener ('submit',
-  async event => {
-  event.preventDefault();
-  const fetchOptions = {
-    method:'POST';
-    body: new FormData(formEl);
-    type: 'multipart/form-data';
-      }
-})
-formEl.addEventListener('submit', inputEl.forEach('input'=>{
-  data[event.target.name]=event.target.value
-}))
-
+formEl.addEventListener('submit',
+  async (event) => {
+    console.log((new FormData(formEl)).get('file'));
+    event.preventDefault();
+    const fetchOptions = {
+      method: 'POST',
+      body: new FormData(formEl),
+      type: 'multipart/form-data',
+    };
+    await fetch(PATH, fetchOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error.message));
+  });
